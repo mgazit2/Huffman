965ad2b97338e1f70ac8b19f4039c9b3307be84a
Matan Gazit <mgazit@Matans-MacBook-Pro.local>
2020-12-05 19:38:06 -0600

    Initial commit

---
+++ src/Huffman.java
@@ -0,0 +1,126 @@
+
+import java.util.*;
+import java.lang.String;
+
+
+class HuffmanNode {
+    int data; // encoded value
+    char c; // key value
+
+    HuffmanNode left; // left subtree
+    HuffmanNode right; // right subtree
+} // end class HuffmanNode
+
+class MyComparator implements Comparator<HuffmanNode> {
+    // compare two nodes
+    public int compare (HuffmanNode x, HuffmanNode y){
+        // return sum for evaluation
+        return x.data - y.data;
+    }
+} // end class MyComparator
+
+public class Huffman {
+    // recursive function prints huffman-code via tree traversal
+    // s is generated huffman code
+
+    public static void CalcFreq(String str, HashMap<Character, Integer> map) {
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            map.merge(c, 1, Integer::sum);
+        }
+    }
+
+    public static void PrintCode(HuffmanNode node, String s){
+        if (node.left == null && node.right == null && Character.isLetter(node.c) || node.c == '-' || node.c == ' ') {
+            System.out.println(node.c + ":" + s);
+            return;
+        }
+        //System.out.print(root.left.c);
+        PrintCode(node.left, s + "0");
+        PrintCode(node.right, s + "1");
+    } // end class Huffman
+
+    public static void main(String[] args){
+        /* Scanner s = new Scanner(System.in); */
+
+        HashMap<Character, Integer> freq = new HashMap<Character,Integer>(); // stores charcter frequency in string
+        String test = "I like-a the poopoo";
+        List <Character> keyList = new ArrayList<Character>();
+        List <Integer> valList = new ArrayList<Integer>();
+        int n = 6;
+        char[] charArray = {'a','b','c','d','e','f'};
+        int[] charFreq = {5,9,12,13,16,45};
+
+        CalcFreq(test, freq);
+
+        for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
+            Character ch = entry.getKey();
+            Integer in = entry.getValue();
+            keyList.add(ch);
+            valList.add(in);
+        }
+
+       /* for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
+            Integer in = entry.getValue();
+            valList.add(in);
+        }*/
+
+        /*System.out.print(freq);
+        System.out.print(keyList);
+        System.out.print(valList);*/
+
+        //create priority queue
+        PriorityQueue<HuffmanNode> q =
+                new PriorityQueue<HuffmanNode>(n, new MyComparator());
+
+        for (int i = 0; i < freq.size(); i++) {
+            HuffmanNode hn = new HuffmanNode();
+
+            hn.c = keyList.get(i);
+            hn.data = valList.get(i);
+            //System.out.print(hn.c);
+
+            hn.left = null;
+            hn.right = null;
+
+            q.add(hn);
+            //System.out.print(q);
+        }
+
+        //System.out.print(q);
+
+        HuffmanNode root = null;
+
+        while (q.size() > 1) {
+
+            // extract first min
+            HuffmanNode x = q.peek();
+            q.poll();
+
+            // extract second min
+            HuffmanNode y = q.peek();
+            q.poll();
+
+            HuffmanNode f = new HuffmanNode();
+
+            // add some frequency of 2 nodes
+            f.data = x.data + y.data;
+            //System.out.print(f.data);
+            f.c = '#';
+            // first extraction to left subtree
+            f.left = x;
+            // second extraction to right subtree
+            f.right = y;
+            // f is now root
+            root = f;
+            // add to this priority queue
+            q.add(f);
+        }
+        if (root.left == null) {
+            System.out.println("Huh?");
+        }
+        //System.out.print(root);
+        //System.out.print(q);
+        PrintCode(root, "");
+    }
+}
